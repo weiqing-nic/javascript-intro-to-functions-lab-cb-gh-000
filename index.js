ad6d7f80f266cdc384f24f95e30727d17a157287
weiqing lee <yoweiqing@gmail.com>
2018-05-29 10:25:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var uppercase = "HELLO!"
+function shout(string){
+  return string.toUpperCase()
+}
 
-uppercase.toUpperCase() === uppercase // true
+function whisper(string){
+  return string.toLowerCase()
+}
 
-var lowercase = 'hello!'
 
-lowercase.toLowerCase() === lowercase // true
+function logWhisper(string){
+  console.log(whisper(string))
+}
 
-var mixedCase = 'Hi there!'
+function logShout(string){
+  console.log(shout(string))
+}
 
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+}
+}
